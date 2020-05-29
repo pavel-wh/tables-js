c50431b6c41c40d6c81f795603756a0c7f2a4296
pB <webforeman@gmail.com>
2020-05-29 06:57:05 +0300

    created component template output

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,21 @@ export class Excel {
 		this.$el = document.querySelector(selector)
 		this.components = options.components || []
 	}
+
+	getRoot() {
+		const $root = document.createElement('div')
+
+		this.components.forEach((Component) => {
+			const component = new Component()
+			$root.insertAdjacentHTML('beforeend', component.toHTML())
+		})
+
+		return $root
+	}
+
+	render() {
+		console.log(this.$el)
+		// afterbegin, afterend, beforeend, beforebegin
+		this.$el.append(this.getRoot())
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import { Excel } from '@/components/excel/Excel'
+import { Header } from '@/components/header/Header'
+import { Toolbar } from '@/components/toolbar/Toolbar'
+import { Formula } from '@/components/formula/Formula'
+import { Table } from '@/components/table/Table'
 import './assets/scss/all'
 
 const excel = new Excel('#app', {
-	components: [],
+	components: [Header, Toolbar, Formula, Table],
 })
 
 console.log('Excel is: ', excel)
+
+excel.render()
